df90140c3db83886d7120ae54d1fc26ec1840d02
Jasmeet Rangar <jasmeetsinghr@gmail.com>
2021-04-01 15:02:07 -0400

    Add about us view

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
-
+  resources :about, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
