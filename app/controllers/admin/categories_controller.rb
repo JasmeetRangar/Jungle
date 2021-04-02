c81d062ade6155e359c39b237561df8e78373aed
Jasmeet Rangar <jasmeetsinghr@gmail.com>
2021-04-02 11:09:03 -0400

    Add admin categories and new category logic, views

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,27 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories]
+    else
+      render :new
+    end
   end
 
-  def create
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
