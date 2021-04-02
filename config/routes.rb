8184e29b171ccaea7800799aac906d373af7e0b5
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-04-02 19:24:26 +0000

    Add users authentication

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,20 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
   resources :about, only: [:index]
+
+  
+
+    # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+  
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create' 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
